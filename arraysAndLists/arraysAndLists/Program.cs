360a56281932674361557e9b013f3ce789961e63
Mmartz1233 <93346422+Mmartz1233@users.noreply.github.com>
2021-12-01 12:38:14 -0800

    Step 242

---
+++ arraysAndLists/arraysAndLists/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+namespace arraysAndLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Trabajando con arrays!");
+            /*******************Creando un array***************/
+            int[] numArray = new int[5];//indicamos de que tamanio
+            numArray[0] = 5; //agregamos valores al array en cada indice
+            numArray[1] = 2;
+            numArray[2] = 10;
+            numArray[3] = 200;
+            numArray[4] = 5000;
+            
+            int[] numArray1 = new int[] { 6, 3, 11, 201, 5001 }; //creamos el array y le agregamos valores, en lugar de hacerlo de la primera forma
+            int[] numArray2 = { 7, 4, 12, 202, 5002, 25 };//creamos un array sin especificar el tipo de dato
+            numArray2[3] = 000; //le estamos diciendo que cambie el valor del indice 3 por 000
+
+            /*******************Creando Listas***************/
+            List<int> intList = new List<int>(); //creando una lista de enteros
+            intList.Add(4);//agregamos valores a la lista
+            intList.Add(10);
+            intList.Add(20);
+            intList.Add(30);
+            intList.Add(40);
+            //intList.Remove(40);
+
+            List<string> listasaludos = new List<string>();
+            listasaludos.Add("Hola");
+            listasaludos.Add("Mayrax");
+            listasaludos.Add("with X :)");
+
+
+
+            Console.WriteLine("valor del array en el indice 3= "+numArray[3]); //imprimimos el valor del array en el indice 3
+            Console.WriteLine("valor del array en el indice 3= "+numArray1[3]);
+            Console.WriteLine("valor del array en el indice 3= "+numArray2[3]);
+            Console.WriteLine("valor de la lista  en el indice 3= " + intList[3]);
+            Console.WriteLine("valor de la lista  en el indice 4 : "+ intList[4]);
+            Console.WriteLine("valor de la lista  en el indice 2: " + listasaludos[2]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ejercicioArray/ejercicioArray/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+namespace ejercicioArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***************Seleccion de Arrays!***********************");
+            Console.WriteLine("selecciona del 1 al 7 cual es tu dia de la semana favorito"); // pedimos al usuario que ingrese datos
+            string dia = Console.ReadLine(); //almacenamos el valor que el usuario ingreso en una variable string
+            
+            string[] numselec = new string[8]; //creamos un array de string
+            numselec[0] = "No existe"; //agregamos valores al array
+            numselec[1] = "Lunes";
+            numselec[2] = "Martes";
+            numselec[3] = "Miercoles";
+            numselec[4] = "Jueves";
+            numselec[5] = "Viernes";
+            numselec[6] = "Sabado";
+            numselec[7] = "Domingo";
+
+            if (dia == "1") //comparamos cada una de las posibles entradas, limitandose a especificamente un numero 
+            {
+                Console.WriteLine(numselec[1]);
+            }else if (dia == "2")
+            {
+                Console.WriteLine(numselec[2]);
+            }else if (dia == "3")
+            {
+                Console.WriteLine(numselec[3]);
+            }else if (dia == "4")
+            {
+                Console.WriteLine(numselec[4]);
+            }else if (dia == "5")
+            {
+                Console.WriteLine(numselec[5]);
+            }else if (dia == "6")
+            {
+                Console.WriteLine(numselec[6]);
+            }else if (dia == "7")
+            {
+                Console.WriteLine(numselec[7]);
+            }
+            else 
+            {
+                Console.WriteLine( "El dia no existe"); // en caso de que no se ingrese lo pedido, monstramos este mensaje
+
+            }
+
+            Console.WriteLine("Selecciona un numero del 1 al 5 "); // pedimos al usuario que ingrese datos
+            int numelegido2 = Convert.ToInt32(Console.ReadLine()); //almacenamos el valor que el usuario ingreso en una variable int
+            int[] numselec2 = new int[6];  //creamos un array de enteros
+            numselec2[0] = 1; //agregamos valores al array
+            numselec2[1] = 10;
+            numselec2[2] = 100;
+            numselec2[3] = 1000;
+            numselec2[4] = 10000;
+            numselec2[5] = 100000;
+            switch (numelegido2) // creamos un switch para comparar las posibles entradas
+            {
+                case 1:
+                    Console.WriteLine(numselec2[1]); //para cada caso mostramos el contenido del array
+                    break;
+                case 2:
+                    Console.WriteLine(numselec2[2]);
+                    break;
+                case 3:
+                    Console.WriteLine(numselec2[3]);
+                    break;
+                case 4:
+                    Console.WriteLine(numselec2[4]);
+                    break;
+                case 5:
+                    Console.WriteLine(numselec2[5]);
+                    break;
+                
+                default:
+                    Console.WriteLine("Del uno al cinco, por favor"); // en caso de que no se ingrese lo pedido, monstramos este mensaje
+                    break;
+            }
+
+
+            Console.WriteLine("***************Seleccion de un elemento de la lista!***********************");
+            Console.WriteLine("Selecciona un numero del 1 al 5 para mostrar la letra correspondiente");// pedimos al usuario que ingrese datos
+            int letrain = Convert.ToInt32(Console.ReadLine()); //almacenamos el valor que el usuario ingreso en una variable int
+            List<char> letra = new List<char>(); //creamos una lista de caracteres
+            letra.Add('-'); //agregamos datos a la lista
+            letra.Add('A');
+            letra.Add('B');
+            letra.Add('C');
+            letra.Add('D'); 
+            letra.Add('E');
+
+            switch (letrain) // creamos un switch para comparar las posibles entradas
+            {
+                case 1:
+                    Console.WriteLine(letra[1]); //para cada caso mostramos el contenido de la lista
+                    break;
+                case 2:
+                    Console.WriteLine(letra[2]);
+                    break;
+                case 3:
+                    Console.WriteLine(letra[3]);
+                    break;
+                case 4:
+                    Console.WriteLine(letra[4]);
+                    break;
+                case 5:
+                    Console.WriteLine(letra[5]);
+                    break;
+
+                default:
+                    Console.WriteLine("Del uno al cinco, por favor :("); // en caso de que no se ingrese lo pedido, monstramos este mensaje
+                    break;
+            }
+
+
+
+
+
+
+
+
+
+
+
+            Console.Read();
+          
+
+        }
+    }
+}
