91a2e4ec489ef39bc315b7e2699eb46229130910
Mmartz1233 <93346422+Mmartz1233@users.noreply.github.com>
2021-12-02 11:50:38 -0800

    Step 256

---
+++ iteracionMore/iteracionMore/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iteracionMore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************LOOP with ARRAYS!****************************");
+            /*ESTE PROGRAMA IMPRIME A TODOS A QUELLOS SUPERIRES A 85 USANDO ARRAYS*/
+            int[] resultadoPrueba = {98,99,85,70,82,34,91,90,94 };
+
+            for (int i = 0; i< resultadoPrueba.Length;i++ ) //Un for pide tres cosas, 1.- donde empiexo? 2.-cuantas veces quiero que se repita? 3.- incrementame conforme a los ciclos
+            {
+                if (resultadoPrueba[i] > 85) // si el valor del indice es mayor a 85
+                {
+                    Console.WriteLine("Los resultados aprovados son: "+ resultadoPrueba[i]); //imprime los resultados mayores a 85
+                }
+            }
+
+
+            string[] nombres = { "May", "Alexis", "Casi", "Dan", "Julio" };
+            for (int j = 0; j<nombres.Length; j++) //Un for pide tres cosas, 1.- donde empiexo? 2.-cuantas veces quiero que se repita? 3.- incrementame conforme a los ciclos
+            {
+                Console.WriteLine(nombres[j]); //si quiero ver todo los que hay en un array
+                if (nombres[j] == "May") // si el valor del indice es migual a May
+                {
+                    
+                    Console.WriteLine(nombres[j]); //imprime los resultados
+                }
+            }
+
+            Console.WriteLine("****************LOOP with LIST!****************************");
+            /*ESTE PROGRAMA IMPRIME A TODOS A QUELLOS SUPERIRES A 85 USANDO LISTAS*/
+            List<int> resptest = new List<int>();
+            resptest.Add(98);
+            resptest.Add(99);
+            resptest.Add(81);
+            resptest.Add(72);
+            resptest.Add(70);
+            foreach (int item in resptest) //un forecha pide dos cosas 1. la variable de iteracion 2.- en donde iterara la variable
+            {
+                if (item > 85)
+                {
+                    Console.WriteLine("Pasaron la prueba: "+item);
+                }
+            }
+
+            /*ESTE PROGRAMA IMPRIME A EL NOMBRE QUE SEA IGUAL A JUL EN UNA LISTA*/
+            List<string> resptest2 = new List<string>() { "May", "Jul", "Alex", "Cas", "Dan" };
+           /* resptest2.Add("May");
+            resptest2.Add("Alex");
+            resptest2.Add("Cas");
+            resptest2.Add("Dan");
+            resptest2.Add("Jul");*/
+            foreach (string item2 in resptest2)
+            {
+                Console.WriteLine(item2);//imprimir todos los lobre de la lista
+                if (item2 == "Jul")
+                {
+                    Console.WriteLine("he is my love" + item2);
+                }
+            }
+
+         
+            /*ESTE PROGRAMA IMPRIME CUANTOS SON SUPERIORES A  85 EN DOS LISTAS*/
+            List<int> testpass = new List<int>() { 98, 99, 12, 74, 23, 23, 99 };// creamos una lista con todos los valores
+            List<int> pasaron = new List<int>(); // creamos una lista con los valores filtrados
+            foreach (int nume in testpass) //hacemos una iteracion en el el testpass
+            {
+                if (nume > 85) //si el valor  de la iteracion es mayor a 85
+                {
+                    pasaron.Add(nume);//agregamos los valores que pasaron a la otra lista
+                }
+            }
+            Console.WriteLine(pasaron.Count);//imprimimos el numero de valores filtrados en la nueva lista
+        }
+    }
+}
---
+++ seisPasos/seisPasos/Program.cs
@@ -0,0 +1,182 @@
+﻿/*
+ Autor Mayra Martz
+ Date 2 dic 2021
+
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace seisPasos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------PASO 1  !-------------------------------");
+            /*Este programa agrega nombres a un array*/
+            string[] nombres = new string[4];
+
+
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine("ingresa un valor");
+                string val = Console.ReadLine();
+
+                nombres[i] = val;
+                Console.WriteLine(nombres[i]);
+
+            }
+
+            Console.WriteLine("--------------------------PASO 2  !-------------------------------");
+            /*Este programa rompe un ciclo infinito utilizando while*/
+            Console.WriteLine("ingresa un numero");
+            int res = Convert.ToInt32(Console.ReadLine());
+           
+            while (res != 5)
+            {
+                Console.WriteLine("ingresa otro numero");
+                res = Convert.ToInt32(Console.ReadLine());
+               // res += 5;
+            }
+
+            Console.WriteLine("--------------------------PASO 3  !-------------------------------");
+            /*Este programa evalua si la entrada por el usuario es correcta y si no lo es, el programa fuerza a que lo haga*/
+            Console.WriteLine("ingresa el operador \" < \" ");
+            string ope = Console.ReadLine();
+            bool comp = ope == "<"; //estoy diciendo que esto es cierto
+           
+            while (!comp)
+            {
+                switch (ope)
+                {
+                    case "<":
+                        Console.WriteLine("Gracias");
+                        comp = true;
+                         break;
+                    default:
+                        Console.WriteLine("Intenta otra vez por favor");
+                        ope = Console.ReadLine();
+                        break;
+
+                }
+            }
+            Console.WriteLine("--------------------------PASO 3 A  !-------------------------------");
+            /*Este programa evalua si la entrada por el usuario es correcta y si no lo es, el programa fuerza a que lo haga utilizando la entraga en consola*/
+            Console.WriteLine("ingresa el operador \" <= \" ");
+            string ope1 = Console.ReadLine();
+            bool comp1 = ope1 == "<="; //estoy diciendo que esto es cierto
+
+            while (!comp1)
+            {
+                switch (ope1)
+                {
+                    case "<=":
+                        Console.WriteLine("Gracias");
+                        comp1 = true;
+                        break;
+                    default:
+                        Console.WriteLine("Intenta otra vez por favor");
+                        ope1 = Console.ReadLine();
+                        break;
+
+                }
+            }
+            
+            Console.WriteLine("--------------------------PASO 4  !-------------------------------");
+            /*Este programa evalua si la entrada por el usuario es correcta y si no lo es, el programa fuerza a que lo haga utilizando una lista*/
+            List<string> diassema = new List<string>() { "", "lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo" };// creamos una lista con todos los valores
+            Console.WriteLine("que dia de la semana no quieres trabajar?");
+            string respuesta = Console.ReadLine();
+            bool romper = respuesta == "";
+            do {
+                switch (respuesta)
+                {
+                    case "lunes":
+                        Console.WriteLine("Elegiste: " + diassema[1]); //para cada caso mostramos el contenido del array
+                        romper = true;
+                        break;
+                    case "martes":
+                        Console.WriteLine("Elegiste: " + diassema[2]);
+                        romper = true;
+                        break;
+                    case "miercoles":
+                        Console.WriteLine("Elegiste: " + diassema[3]);
+                        romper = true;
+                        break;
+                    case "jueves":
+                        Console.WriteLine("Elegiste: " + diassema[4]);
+                        romper = true;
+                        break;
+                    case "viernes":
+                        Console.WriteLine("Elegiste: " + diassema[5]);
+                        romper = true;
+                        break;
+                    case "sabado":
+                        Console.WriteLine("Elegiste: " + diassema[6]);
+                        romper = true;
+                        break;
+                    case "domingo":
+                        Console.WriteLine("Elegiste: " + diassema[7]);
+                        romper = true;
+                         break;
+
+                    default:
+                        Console.WriteLine("Dia de la semana, por favor"); // en caso de que no se ingrese lo pedido, monstramos este mensaje
+                        respuesta = Console.ReadLine();
+
+                        break;
+                }//fin swith
+            } while (!romper);
+           
+            Console.WriteLine("--------------------------PASO 5  !-------------------------------");
+            /*Este programa evalua si la entrada por el usuario es correcta y y si elige la palabra repetida el combo es doble*/
+            List<string> comidas = new List<string>() { "", "milanesa", "mole", "mole", "tacos" };// creamos una lista con todos los valores
+            Console.WriteLine("que quieres comer?");
+            string orden = Console.ReadLine();
+            bool romper1 = orden == "";
+            do
+            {
+                switch (orden)
+                {
+                    case "milanesa":
+                        Console.WriteLine("Elegiste: " + comidas[1]); //para cada caso mostramos el contenido del array
+                        romper1 = true;
+                        break;
+                    case "mole":
+                        Console.WriteLine("tu orden " + comidas[2] );//+ " es doble!!" + comidas[3]
+                        foreach (string index in comidas)
+                        {
+                            if (index == "mole")
+                            {
+                                Console.WriteLine( index);
+                            }
+                        }
+                        romper1 = true;
+                        break;
+                    case "tacos":
+                        Console.WriteLine("Elegiste: " + comidas[4]);
+                        romper1 = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Que este en el menu, por favor"); // en caso de que no se ingrese lo pedido, monstramos este mensaje
+                        orden = Console.ReadLine();
+
+                        break;
+                }//fin swith
+            } while (!romper1);
+           
+           Console.WriteLine("--------------------------PASO 6  !-------------------------------");
+            /*Este programa muestra el menu de bebidas utilizando un for each*/
+            List<string> bebidas = new List<string>() { "agua", "jugo", "soda", "soda", "mezcal" };
+            foreach (string index in bebidas)
+            {
+                Console.WriteLine("las bebidas son :"+index);
+                
+            }
+        
+           Console.Read();
+        }
+    }
+}
