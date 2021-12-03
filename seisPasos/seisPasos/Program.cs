4768f517a16f40de670189047ded923a8ba1a71f
Mmartz1233 <93346422+Mmartz1233@users.noreply.github.com>
2021-12-02 22:23:03 -0800

    Step 256 correcciones

--- seisPasos/seisPasos/Program.cs
+++ seisPasos/seisPasos/Program.cs
@@ -12,7 +12,8 @@ namespace seisPasos
     class Program
     {
         static void Main(string[] args)
-        {    
+        {
+            
             Console.WriteLine("--------------------------PASO 1  !-------------------------------");
             //Este programa agrega nombres a un array
             string[] nombres = new string[] {"Mayra ", "Julio ","Casi "};
@@ -42,32 +43,20 @@ namespace seisPasos
             }
             
             Console.WriteLine("--------------------------PASO 3  !-------------------------------");
-            //Este programa evalua si la entrada por el usuario es correcta y si no lo es, el programa fuerza a que lo haga
-            Console.WriteLine("ingresa un numero menor a 10 ");
-            int  ope = Convert.ToInt32( Console.ReadLine());
-            // bool comp = ope == "<"; //estoy diciendo que esto es cierto
-          
-            while (ope < 10)
+            //Este programa  utiliza el operador menor que o menor o igual que
+            int j = 0;
+            while (j < 10)                                             
             {
-                Console.WriteLine("ingresa otro numero");
-                ope = Convert.ToInt32(Console.ReadLine());
-                // res += 5;
+                Console.WriteLine("num: "+ j);
+                j++;
             }
 
-            
-            Console.WriteLine("--------------------------PASO 3 A  !-------------------------------");
-            //Este programa evalua si la entrada por el usuario es correcta y si no lo es, el programa fuerza a que lo haga utilizando la entraga en consola
-            Console.WriteLine("ingresa un numero menor o igual a 10 ");
-            int ope1 = Convert.ToInt32(Console.ReadLine());
-            // bool comp = ope == "<"; //estoy diciendo que esto es cierto
-
-            while (ope1 <= 10)
+            for (int i = 0; i <= 10; i++)                                
             {
-                Console.WriteLine("ingresa otro numero");
-                ope1 = Convert.ToInt32(Console.ReadLine());
-                // res += 5;
+                Console.WriteLine("num: " + i);
             }
             
+           
             Console.WriteLine("--------------------------PASO 4  !-------------------------------");
             //Este programa evalua si la entrada por el usuario es correcta y si no lo es, el programa fuerza a que lo haga utilizando una lista
             List<string> diassema = new List<string>() { "", "lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo" };// creamos una lista con todos los valores
@@ -117,50 +106,39 @@ namespace seisPasos
            
             Console.WriteLine("--------------------------PASO 5  !-------------------------------");
             //Este programa evalua si la entrada por el usuario es correcta y y si elige la palabra repetida el combo es doble
-            List<string> comidas = new List<string>() { "", "milanesa", "mole", "mole", "tacos" };// creamos una lista con todos los valores
-            Console.WriteLine("que quieres comer?");
+            List<int> coincidencia = new List<int>();
+            List<string> comida = new List<string>(); 
+            comida.Add("mole");                                                   
+            comida.Add("barbacoa");
+            comida.Add("atun");
+            Console.WriteLine("Ingresa tu pedido de comida: ");
             string orden = Console.ReadLine();
-
-            string var0 = comidas[0];
-            var0 = "1";
-            string var1 = comidas[1];
-            var1 = "2";
-            string var2 = comidas[2];
-            var2 = "3";
-            string var3 = comidas[3];
-            var3 = "4";
-            string var4 = comidas[4];
-            var4 = "5";
-
-            bool romper1 = orden == "";
-            do
-            {
-                switch (orden)
-                {
-                    case "milanesa":
-                        Console.WriteLine("Elegiste: " + comidas[1]); //para cada caso mostramos el contenido del array
-                        romper1 = true;
-                        break;
-                    case "mole":
-                        Console.WriteLine("tu orden " + comidas[2]);//+ " es doble!!" + comidas[3]
-                        Console.WriteLine(var2 + " y " + var3);
-                        romper1 = true;
-                        break;
-                    case "tacos":
-                        Console.WriteLine("Elegiste: " + comidas[4]);
-                        romper1 = true;
-                        break;
-
-                    default:
-                        Console.WriteLine("Que este en el menu, por favor"); // en caso de que no se ingrese lo pedido, monstramos este mensaje
-                        orden = Console.ReadLine();
-
-                        break;
-                }//fin swith
-            } while (!romper1);
-
-
+            bool oredenencontrada = false;
             
+            while (true)
+            {
+               
+                for (int i = 0; i < comida.Count; i++)                                      
+                {
+                    if (comida[i].Equals(orden))                      
+                    {
+                        oredenencontrada = true;
+                        coincidencia.Add(i);
+                    }
+                }
+
+                if (oredenencontrada)                                                             
+                {
+                    Console.WriteLine("posicion: " + String.Join(", ", coincidencia.ToArray()));
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Intenta otra vez ; ");
+                    orden = Console.ReadLine();
+                }
+            }
+
             Console.WriteLine("--------------------------PASO 6  !-------------------------------");
              //Este programa muestra el menu de bebidas utilizando un for each
              List<string> bebidas = new List<string>() { "agua", "jugo", "soda", "soda", "mezcal" };
