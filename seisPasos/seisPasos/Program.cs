ef14ef68e9b18f8497c0574eba59b6568f148664
Mmartz1233 <93346422+Mmartz1233@users.noreply.github.com>
2021-12-02 16:37:44 -0800

    Step 256 correccion

--- seisPasos/seisPasos/Program.cs
+++ seisPasos/seisPasos/Program.cs
@@ -12,10 +12,11 @@ namespace seisPasos
     class Program
     {
         static void Main(string[] args)
-        {
+        {    
             Console.WriteLine("--------------------------PASO 1  !-------------------------------");
-            /*Este programa agrega nombres a un array*/
-            string[] nombres = new string[4];
+            //Este programa agrega nombres a un array
+            string[] nombres = new string[] {"Mayra ", "Julio ","Casi "};
+            
 
 
             for (int i = 0; i < nombres.Length; i++)
@@ -23,13 +24,13 @@ namespace seisPasos
                 Console.WriteLine("ingresa un valor");
                 string val = Console.ReadLine();
 
-                nombres[i] = val;
+                nombres[i] += val;
                 Console.WriteLine(nombres[i]);
 
             }
-
+            
             Console.WriteLine("--------------------------PASO 2  !-------------------------------");
-            /*Este programa rompe un ciclo infinito utilizando while*/
+            //Este programa rompe un ciclo infinito utilizando while
             Console.WriteLine("ingresa un numero");
             int res = Convert.ToInt32(Console.ReadLine());
            
@@ -39,52 +40,36 @@ namespace seisPasos
                 res = Convert.ToInt32(Console.ReadLine());
                // res += 5;
             }
-
+            
             Console.WriteLine("--------------------------PASO 3  !-------------------------------");
-            /*Este programa evalua si la entrada por el usuario es correcta y si no lo es, el programa fuerza a que lo haga*/
-            Console.WriteLine("ingresa el operador \" < \" ");
-            string ope = Console.ReadLine();
-            bool comp = ope == "<"; //estoy diciendo que esto es cierto
-           
-            while (!comp)
+            //Este programa evalua si la entrada por el usuario es correcta y si no lo es, el programa fuerza a que lo haga
+            Console.WriteLine("ingresa un numero menor a 10 ");
+            int  ope = Convert.ToInt32( Console.ReadLine());
+            // bool comp = ope == "<"; //estoy diciendo que esto es cierto
+          
+            while (ope < 10)
             {
-                switch (ope)
-                {
-                    case "<":
-                        Console.WriteLine("Gracias");
-                        comp = true;
-                         break;
-                    default:
-                        Console.WriteLine("Intenta otra vez por favor");
-                        ope = Console.ReadLine();
-                        break;
-
-                }
+                Console.WriteLine("ingresa otro numero");
+                ope = Convert.ToInt32(Console.ReadLine());
+                // res += 5;
             }
+
+            
             Console.WriteLine("--------------------------PASO 3 A  !-------------------------------");
-            /*Este programa evalua si la entrada por el usuario es correcta y si no lo es, el programa fuerza a que lo haga utilizando la entraga en consola*/
-            Console.WriteLine("ingresa el operador \" <= \" ");
-            string ope1 = Console.ReadLine();
-            bool comp1 = ope1 == "<="; //estoy diciendo que esto es cierto
+            //Este programa evalua si la entrada por el usuario es correcta y si no lo es, el programa fuerza a que lo haga utilizando la entraga en consola
+            Console.WriteLine("ingresa un numero menor o igual a 10 ");
+            int ope1 = Convert.ToInt32(Console.ReadLine());
+            // bool comp = ope == "<"; //estoy diciendo que esto es cierto
 
-            while (!comp1)
+            while (ope1 <= 10)
             {
-                switch (ope1)
-                {
-                    case "<=":
-                        Console.WriteLine("Gracias");
-                        comp1 = true;
-                        break;
-                    default:
-                        Console.WriteLine("Intenta otra vez por favor");
-                        ope1 = Console.ReadLine();
-                        break;
-
-                }
+                Console.WriteLine("ingresa otro numero");
+                ope1 = Convert.ToInt32(Console.ReadLine());
+                // res += 5;
             }
             
             Console.WriteLine("--------------------------PASO 4  !-------------------------------");
-            /*Este programa evalua si la entrada por el usuario es correcta y si no lo es, el programa fuerza a que lo haga utilizando una lista*/
+            //Este programa evalua si la entrada por el usuario es correcta y si no lo es, el programa fuerza a que lo haga utilizando una lista
             List<string> diassema = new List<string>() { "", "lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo" };// creamos una lista con todos los valores
             Console.WriteLine("que dia de la semana no quieres trabajar?");
             string respuesta = Console.ReadLine();
@@ -93,31 +78,32 @@ namespace seisPasos
                 switch (respuesta)
                 {
                     case "lunes":
-                        Console.WriteLine("Elegiste: " + diassema[1]); //para cada caso mostramos el contenido del array
+                        Console.WriteLine("Es el dia : " +diassema.IndexOf("lunes"));
+                        
                         romper = true;
                         break;
                     case "martes":
-                        Console.WriteLine("Elegiste: " + diassema[2]);
+                        Console.WriteLine("Es el dia : " + diassema.IndexOf("martes"));
                         romper = true;
                         break;
                     case "miercoles":
-                        Console.WriteLine("Elegiste: " + diassema[3]);
+                        Console.WriteLine("Es el dia : " + diassema.IndexOf("miercoles"));
                         romper = true;
                         break;
                     case "jueves":
-                        Console.WriteLine("Elegiste: " + diassema[4]);
+                        Console.WriteLine("Es el dia : " + diassema.IndexOf("jueves"));
                         romper = true;
                         break;
                     case "viernes":
-                        Console.WriteLine("Elegiste: " + diassema[5]);
+                        Console.WriteLine("Es el dia : " + diassema.IndexOf("viernes"));
                         romper = true;
                         break;
                     case "sabado":
-                        Console.WriteLine("Elegiste: " + diassema[6]);
+                        Console.WriteLine("Es el dia : " + diassema.IndexOf("sabado"));
                         romper = true;
                         break;
                     case "domingo":
-                        Console.WriteLine("Elegiste: " + diassema[7]);
+                        Console.WriteLine("Es el dia : " + diassema.IndexOf("domingo"));
                         romper = true;
                          break;
 
@@ -130,10 +116,22 @@ namespace seisPasos
             } while (!romper);
            
             Console.WriteLine("--------------------------PASO 5  !-------------------------------");
-            /*Este programa evalua si la entrada por el usuario es correcta y y si elige la palabra repetida el combo es doble*/
+            //Este programa evalua si la entrada por el usuario es correcta y y si elige la palabra repetida el combo es doble
             List<string> comidas = new List<string>() { "", "milanesa", "mole", "mole", "tacos" };// creamos una lista con todos los valores
             Console.WriteLine("que quieres comer?");
             string orden = Console.ReadLine();
+
+            string var0 = comidas[0];
+            var0 = "1";
+            string var1 = comidas[1];
+            var1 = "2";
+            string var2 = comidas[2];
+            var2 = "3";
+            string var3 = comidas[3];
+            var3 = "4";
+            string var4 = comidas[4];
+            var4 = "5";
+
             bool romper1 = orden == "";
             do
             {
@@ -144,14 +142,8 @@ namespace seisPasos
                         romper1 = true;
                         break;
                     case "mole":
-                        Console.WriteLine("tu orden " + comidas[2] );//+ " es doble!!" + comidas[3]
-                        foreach (string index in comidas)
-                        {
-                            if (index == "mole")
-                            {
-                                Console.WriteLine( index);
-                            }
-                        }
+                        Console.WriteLine("tu orden " + comidas[2]);//+ " es doble!!" + comidas[3]
+                        Console.WriteLine(var2 + " y " + var3);
                         romper1 = true;
                         break;
                     case "tacos":
@@ -166,17 +158,28 @@ namespace seisPasos
                         break;
                 }//fin swith
             } while (!romper1);
-           
-           Console.WriteLine("--------------------------PASO 6  !-------------------------------");
-            /*Este programa muestra el menu de bebidas utilizando un for each*/
-            List<string> bebidas = new List<string>() { "agua", "jugo", "soda", "soda", "mezcal" };
-            foreach (string index in bebidas)
+
+
+            
+            Console.WriteLine("--------------------------PASO 6  !-------------------------------");
+             //Este programa muestra el menu de bebidas utilizando un for each
+             List<string> bebidas = new List<string>() { "agua", "jugo", "soda", "soda", "mezcal" };
+             List<string> nueva = new List<string>(); // creamos una lista con los valores filtrados
+             foreach (string index in bebidas)
             {
-                Console.WriteLine("las bebidas son :"+index);
-                
+                nueva.Add(index);
+                if (nueva.Contains(index))
+                {
+                    
+                    Console.WriteLine("Valores agregados " + index);
+                }else if (!nueva.Contains(index))
+                {
+                    Console.WriteLine("Lista vacia");
+                }
             }
-        
-           Console.Read();
+             
+            
+            Console.Read();
         }
     }
 }
