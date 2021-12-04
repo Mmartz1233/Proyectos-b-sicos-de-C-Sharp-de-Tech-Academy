f08cf3fb365e58d28cf7874e29b7047e0fcc3c0a
Mmartz1233 <93346422+Mmartz1233@users.noreply.github.com>
2021-12-04 00:45:10 -0800

    Step 275

---
+++ asignacionMetodos/asignacionMetodos/Operaciones.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace asignacionMetodos
+{
+    class Operaciones //Crear una clase.
+    {
+        //cree tres métodos, cada uno de los cuales tomará un parámetro de número entero y devolverá un número entero
+        public static int Sumar(int num1, int num2)
+        {
+            return num1 + num2;
+
+        }
+        public static int Restar(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static int Multiplicar(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
---
+++ asignacionMetodos/asignacionMetodos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace asignacionMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // En el programa Main(), pregunte al usuario en qué número desea realizar las operaciones matemáticas.
+           Console.WriteLine("Sumar - Restar - Multiplicar");
+            //Llame a cada método a su vez, pasando la entrada del usuario al método. Muestra el entero devuelto en la pantalla.
+            Console.WriteLine("Ingresa un numero");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingresa otro numero");
+            int val2 = Convert.ToInt32(Console.ReadLine());
+
+            int suma = Operaciones.Sumar(val1, val2);
+            int resta = Operaciones.Restar(val1, val2);
+            int mul= Operaciones.Multiplicar(val1, val2);
+
+            Console.WriteLine(" La suma es: "+suma+ " la resta es: "+resta+ " la multiplicacion es: "+mul);
+
+        }
+    }
+}
