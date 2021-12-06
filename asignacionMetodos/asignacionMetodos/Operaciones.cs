2539864eb68015347e9ab7c6f0f1b23214914fb4
Mmartz1233 <93346422+Mmartz1233@users.noreply.github.com>
2021-12-06 12:47:37 -0800

    correccion 275

--- asignacionMetodos/asignacionMetodos/Operaciones.cs
+++ asignacionMetodos/asignacionMetodos/Operaciones.cs
@@ -7,18 +7,18 @@ namespace asignacionMetodos
     class Operaciones //1. Crear una clase.
     {
         //1. cree tres métodos, cada uno de los cuales tomará un parámetro de número entero y devolverá un número entero
-        public static int Sumar(int num1, int num2)
+        public static int Sumar(int num1)
         {
-            return num1 + num2;
+            return num1 + 7;
 
         }
-        public static int Restar(int num1, int num2)
+        public static int Restar(int num1)
         {
-            return num1 - num2;
+            return num1 - 7;
         }
-        public static int Multiplicar(int num1, int num2)
+        public static int Multiplicar(int num1)
         {
-            return num1 * num2;
+            return num1 * 7;
         }
     }
 }
