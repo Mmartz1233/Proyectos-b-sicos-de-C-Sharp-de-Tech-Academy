9609d3892cd746a829f54362ac5e9e447e4ec309
Mmartz1233 <93346422+Mmartz1233@users.noreply.github.com>
2021-12-05 22:46:08 -0800

    step 285

---
+++ usandoClasesStaticas/usandoClasesStaticas/Operaciones.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace usandoClasesStaticas
+{
+    class Operaciones//step 1 .- Crear una clase
+    {
+        //step 1.- En esa clase, cree un método void que genere un número entero. Haga que el método divida los datos que se le pasan por 2.
+        public void Division(int a)
+        {
+            int div = a/ 2;
+            Console.WriteLine("El numero es: "+ a);
+            Console.WriteLine("La division es: "+ div);
+        }
+        //step 4 .- Cree un método con parámetros de salida.
+        public int Suma(int b)
+        {
+            return b + b;
+        }
+        //step 5 .- Sobrecargar un método.
+        public int Suma(int b, int a)
+        {
+            return b + a * b;
+        }
+
+        //Declara una clase estática.
+        public static void Static()
+        {
+            Console.WriteLine("Soy estatica");
+        }
+    }
+}
---
+++ usandoClasesStaticas/usandoClasesStaticas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace usandoClasesStaticas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //step 2 .- En el método Main (), cree una instancia de esa clase.
+            Operaciones op = new Operaciones();
+            //step 3.- Haga que el usuario ingrese un número. Llame al método en ese número. Muestre la salida en la pantalla. Debe ser el número ingresado, dividido por dos.
+            Console.WriteLine("Ingrese un numero");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            op.Division(num);
+            //lamando metodos:
+            
+            Console.WriteLine("Suma de un metodo: "+op.Suma(num));
+            Console.WriteLine("Sobrecarga de metodos: "+op.Suma(num,num1));
+            Operaciones.Static();
+            
+
+        }
+    }
+}
