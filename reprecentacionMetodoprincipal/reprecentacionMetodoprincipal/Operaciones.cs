2b798e17219660487a0a58d582387d00877fe154
Mmartz1233 <93346422+Mmartz1233@users.noreply.github.com>
2021-12-06 11:34:58 -0800

    Correccion Step 279

--- reprecentacionMetodoprincipal/reprecentacionMetodoprincipal/Operaciones.cs
+++ reprecentacionMetodoprincipal/reprecentacionMetodoprincipal/Operaciones.cs
@@ -8,22 +8,22 @@ namespace reprecentacionMetodoprincipal
     {
         //sobre carga de metodos: crear un metodo con el mismo nombre pero con diferentes atributos, el tipo de retorno puede cambiar 
         //step1.- cree un método que tome un número entero, cree una operación matemática para este número entero (suma, resta, etc.), luego devuelva la respuesta como un número entero.
-        public static int Suma(int a, int b)
+        public  int Suma(int a)
         {
-            return a + b;
+            return a + a;
         }
         //step 3 .- Agregue un segundo método a la clase con el mismo nombre  que tomará un decimal, cree una operación matemática diferente para él y luego devuelva la respuesta como un número entero.
-        public static int Suma(decimal x, decimal y)
+        public  int Suma(decimal x)
         {
             //de decimal a int
             int equis = Convert.ToInt32(x);
-            int ye = Convert.ToInt32(y);
-            return equis - ye;
+          
+            return equis - equis;
         }
         //step 5.- Agregue un tercer método a la clase, con el mismo nombre, que tomará una cadena, la convertirá a un número entero si es posible, le hará una operación matemática diferente y luego devolverá la respuesta como un número entero.
-        public static int Suma(string letra1, string letra2)
+        public  int Suma(string letra1)
         {
-          return Int32.Parse(letra1) * Int32.Parse(letra2);
+          return Int32.Parse(letra1) * Int32.Parse(letra1);
         }
        
     }
