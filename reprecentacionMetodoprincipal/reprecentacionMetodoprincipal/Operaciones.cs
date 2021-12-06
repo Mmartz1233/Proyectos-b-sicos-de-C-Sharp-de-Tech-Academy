b6f3dba01ff6d2fc8401b298c1b5e8d9b24689b9
Mmartz1233 <93346422+Mmartz1233@users.noreply.github.com>
2021-12-05 21:01:29 -0800

    step 279

---
+++ reprecentacionMetodoprincipal/reprecentacionMetodoprincipal/Operaciones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace reprecentacionMetodoprincipal
+{
+    class Operaciones //step 1.- Crear una clase.
+    {
+        //sobre carga de metodos: crear un metodo con el mismo nombre pero con diferentes atributos, el tipo de retorno puede cambiar 
+        //step1.- cree un método que tome un número entero, cree una operación matemática para este número entero (suma, resta, etc.), luego devuelva la respuesta como un número entero.
+        public static int Suma(int a, int b)
+        {
+            return a + b;
+        }
+        //step 3 .- Agregue un segundo método a la clase con el mismo nombre  que tomará un decimal, cree una operación matemática diferente para él y luego devuelva la respuesta como un número entero.
+        public static int Suma(decimal x, decimal y)
+        {
+            //de decimal a int
+            int equis = Convert.ToInt32(x);
+            int ye = Convert.ToInt32(y);
+            return equis - ye;
+        }
+        //step 5.- Agregue un tercer método a la clase, con el mismo nombre, que tomará una cadena, la convertirá a un número entero si es posible, le hará una operación matemática diferente y luego devolverá la respuesta como un número entero.
+        public static int Suma(string letra1, string letra2)
+        {
+            //conbierta a un numero
+            //realice una operacion matematica en un entero
+            int num1 = Convert.ToInt32(letra1);
+            int num2 = Convert.ToInt32(letra2);
+            int mult = num1 * num2;
+            string multi = Convert.ToString(mult);
+            return Int32.Parse(letra1) * Int32.Parse(letra2);
+        }
+       
+    }
+}
---
+++ reprecentacionMetodoprincipal/reprecentacionMetodoprincipal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace reprecentacionMetodoprincipal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingresa un numero: ");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa otro numero: ");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            Operaciones op = new Operaciones();
+
+            //step 2 .- cree una instancia de la clase y llame al método one, pasando un número entero
+            int suma = Operaciones.Suma(numero1, numero2);
+            Console.WriteLine(" la suma en el primer metodod es: "+suma);
+
+
+            //step 3.- En el método Main () de la aplicación de consola, cree una instancia de la clase y llame al segundo método, pasando un decimal
+            decimal dec1 = numero1;
+            decimal dec2 = numero2;
+            decimal suma2 = Operaciones.Suma(dec1, dec2);
+            Console.WriteLine(" la resta del segundo metodo es: "+suma2);
+
+
+            //step 6.- En el método Main () de la aplicación de consola, cree una instancia de la clase y llame al tercer método, pasando una cadena que equivale a un número entero.
+            string num1 = Convert.ToString(numero1);
+            string num2 = Convert.ToString(numero2);
+            int suma4 = Operaciones.Suma(num1, num2);
+            Console.WriteLine("La multiplicacion del tercer metodo es: " +suma4);
+        }
+
+      
+
+    }
+}
