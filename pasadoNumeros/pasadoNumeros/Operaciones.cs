2e89d1cac5ca7675a73e80eb2f93140616ca25ac
Mmartz1233 <93346422+Mmartz1233@users.noreply.github.com>
2021-12-05 21:36:16 -0800

    step 280

---
+++ pasadoNumeros/pasadoNumeros/Operaciones.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pasadoNumeros
+{
+    class Operaciones//step 1.- Crear una clase.
+    {
+        //step1.- En esa clase, cree un método que tome dos números enteros como parámetros. Haz que uno de ellos sea opcional. Haga que el método realice una operación matemática y devuelva un resultado entero.
+        public  int operacion1 (int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ pasadoNumeros/pasadoNumeros/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace pasadoNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //step 2.- En el método Main () de la aplicación de consola, cree una instancia de la clase.
+            Operaciones op = new Operaciones();
+            //step 3 .- Pídale al usuario que ingrese dos números, uno a la vez. Hágales saber que no necesitan ingresar nada para el segundo número.
+            Console.WriteLine("Ingresa un numero: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa otro numero: (este es opcional)");
+            try
+            {
+                int y = Convert.ToInt32(Console.ReadLine());
+
+                //step 4 .- Llame al método en la clase, pasando uno o dos números ingresados.
+                int res = op.operacion1(x, y);
+                Console.WriteLine("el resultado es: " + res);
+            }
+            catch (Exception)
+            {
+                int res = op.operacion1(x, x);
+                Console.WriteLine("el resultado es: " + res);
+
+            }
+        }
+    }
+}
