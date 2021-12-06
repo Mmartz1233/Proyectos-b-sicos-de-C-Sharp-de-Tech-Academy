46c2b494ef8e2d5738b0039983e51d570023983c
Mmartz1233 <93346422+Mmartz1233@users.noreply.github.com>
2021-12-05 22:08:28 -0800

    step 281

---
+++ usandoVoid/usandoVoid/Operaciones.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace usandoVoid
+{
+    class Operaciones //step 1.- Crear una clase.
+    {
+        //step 1 .- cree un método void que tome dos números enteros como parámetros. Haga que el método realice una operación matemática en el primer número entero y muestre el segundo número entero en la pantalla.
+        public void Suma(int a, int b)
+        {
+            int suma = a + b;
+            Console.WriteLine("el segundo numero es: "+b);
+            Console.WriteLine("la suma de los dos numeros son: "+suma);
+        }
+    }
+}
---
+++ usandoVoid/usandoVoid/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace usandoVoid
+{
+    class Program
+    {
+        static void Main(string[] args) //step 2.- En el método Main () de la aplicación de consola, cree una instancia de la clase
+        {
+            Console.WriteLine("Hello World!");
+            Operaciones op = new Operaciones();
+            Console.WriteLine("Ingresa un numero");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa otros numero");
+             int b = Convert.ToInt32(Console.ReadLine());
+            op.Suma(a, b); //step 3 y 4.- Llame al método en la clase, pasando dos números, Llame al método en la clase, especificando los parámetros por nombre.
+        }
+    }
+}
