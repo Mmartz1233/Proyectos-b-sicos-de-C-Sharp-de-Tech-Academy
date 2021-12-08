b2626c9fd2f11d138c2b71f0da096b76b98dd690
Mmartz1233 <93346422+Mmartz1233@users.noreply.github.com>
2021-12-08 12:25:28 -0800

    step 324

---
+++ asignacionEnums/asignacionEnums/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace asignacionEnums
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Utilizando Enum");
+            Semana sem = new Semana();
+            sem.Dm = DiasSemana.lunes;
+            //paso 3.- //Asigne el valor a una variable de ese tipo de datos de enumeración que acaba de crear.
+            string dia1 = Convert. ToString(DiasSemana.lunes);
+            //si lo quiero convertir a entero
+            //int dia11 = Convert.ToInt32(DiasSemana.lunes);
+            //Console.WriteLine(" otro: "+dia11);
+
+            //paso 4.- Envuelva la declaración anterior en un bloque try / catch y haga que se imprima "Ingrese un día real de la semana". a la consola si ocurre un error.
+            try
+            {
+                Console.WriteLine("Ingresa un dia de la semana");
+               
+                String diaingresado = Console.ReadLine();
+                //paso 5.- Convierte la representación de cadena del nombre o valor numérico de una o más constantes enumeradas en un objeto enumerado equivalente.
+                DiasSemana diade = (DiasSemana)Enum.Parse(typeof(DiasSemana), diaingresado);     
+                Console.WriteLine("el dia que ingresaste es: "+diade);       
+                                              
+            }
+            catch (ArgumentException)                                                       
+            {
+                Console.WriteLine("Ingrese un día real de la semana");
+            }
+
+
+
+
+
+        }
+
+    }
+}
---
+++ asignacionEnums/asignacionEnums/Semana.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace asignacionEnums
+{
+    public class Semana
+    {
+        //creando propiedades
+        public DiasSemana Dm { get; set; }
+
+
+    }
+    //PASO 1.- Cree una enumeración para los días de la semana.
+    public enum DiasSemana
+    {
+        lunes,
+        martes,
+        miercoles,
+        jueves,
+        viernes,
+        sabado,
+        domingo
+    }
+}
